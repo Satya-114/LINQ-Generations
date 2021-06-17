2198f71076fb49a571e02fce47ae65248b379cd2
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-06-17 17:30:19 +0530

    Add files via upload

---
+++ Repeat/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ_Repeat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<string> repeatStrings = Enumerable.Repeat("Welcome to DOT NET Tutorials", 10);
+            foreach (String str in repeatStrings)
+            {
+                Console.WriteLine(str);
+            }
+            Console.ReadKey();
+        }
+    }
+}
