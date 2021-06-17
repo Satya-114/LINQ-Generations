4cd9a8515cf5f7ed48f8aecc78a3c30c0d732c14
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-06-17 17:28:37 +0530

    Add files via upload

---
+++ Range/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ_Range
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<int> numberSequence = Enumerable.Range(1, 10);
+            foreach (int i in numberSequence)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
