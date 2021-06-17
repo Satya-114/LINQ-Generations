053cbb7068cc045f47fe5c8a249c41c3c0ad8885
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-06-17 17:32:22 +0530

    Add files via upload

---
+++ Empty/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ_Empty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var emptyCollection1 = Enumerable.Empty<string>();
+            var emptyCollection2 = Enumerable.Empty<Student>();
+            Console.WriteLine("Count: {0} ", emptyCollection1.Count());
+            Console.WriteLine("Type: {0} ", emptyCollection1.GetType().Name);
+            Console.WriteLine("Count: {0} ", emptyCollection2.Count());
+            Console.WriteLine("Type: {0} ", emptyCollection2.GetType().Name);
+            Console.ReadKey();
+        }
+    }
+    public class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+    }
+
